25c6dd9fa6be79c2e343c1885806ab3344aa739a
정민혁 <jlssteeve1@gmail.com>
2022-01-16 08:54:38 +0900

    Fix intent filter

--- app/src/main/java/com/pss/highthon_android/base/BaseActivity.kt
+++ app/src/main/java/com/pss/highthon_android/base/BaseActivity.kt
@@ -32,4 +32,5 @@ abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutRe
 
     protected fun longShowToast(msg: String) =
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+
 }
