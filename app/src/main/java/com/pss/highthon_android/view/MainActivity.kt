03c2a952e2d3a34b2595ab313ad233a96dc77838
ParkSangSun <appleasz1028102@gmail.com>
2022-01-15 16:37:12 +0900

    add : bottom nav bar

--- app/src/main/java/com/pss/highthon_android/view/MainActivity.kt
+++ app/src/main/java/com/pss/highthon_android/view/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.pss.highthon_android.view
 
 import androidx.activity.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.pss.highthon_android.R
 import com.pss.highthon_android.base.BaseActivity
 import com.pss.highthon_android.databinding.ActivityMainBinding
@@ -13,6 +16,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
 
     override fun init() {
+        initBottomNavBar()
+    }
 
+    private fun initBottomNavBar() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.nav_host)?.findNavController()
+        val nav = binding.bottomNav as BottomNavigationView
+        navController?.let {
+            nav.setupWithNavController(navController)
+        }
     }
 }
---
+++ app/src/main/java/com/pss/highthon_android/view/UploadFragment.kt
@@ -0,0 +1,18 @@
+package com.pss.highthon_android.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.highthon_android.R
+import com.pss.highthon_android.base.BaseFragment
+import com.pss.highthon_android.databinding.FragmentUploadBinding
+
+
+class UploadFragment : BaseFragment<FragmentUploadBinding>(R.layout.fragment_upload) {
+
+    override fun init() {
+
+    }
+}
